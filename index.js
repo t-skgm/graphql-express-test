6c5d0f424664e554f1b09773a82ea5a828171baa
t-skgm <skgm0301@gmail.com>
2018-08-24 16:53:25 +0900

    add monsters db

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { ApolloServer, gql } = require('apollo-server-express');
-const { makeExecutableSchema } = require("graphql-tools");
-
+const { makeExecutableSchema } = require('graphql-tools');
 // Mock data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: "J.K. Rowling",
-    price: 2000
-  },
-  {
-    title: "Jurassic Park",
-    author: "Michael Crichton",
-    price: 3000
-  }
-];
+const monsters = require('./database.json');
 
 const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String, price: Int }
+  type Monster { id: Int, name: String, img_src: String }
+  type Query {
+    monsters: [Monster]
+    monster(id: Int): Monster
+    maxMonsterId: Int
+  }
 `;
 
 const resolvers = {
-  Query: { books: () => books }
+  Query: {
+    monsters: () => monsters,
+    monster: (obj, args, context, info) => _.find(monsters, { id: args.id }),
+    maxMonsterId: () => _.max(_.map(monsters, m => m.id))
+  }
 };
 
 const schema = makeExecutableSchema({
